3aa2f9851d5cf7b302f0b26471dc659a99bff2c6
Bartłomiej Błazik <Bartomiej Bazik>
2016-10-02 12:34:12 +0200

    VirtualState refactor done

--- Sokoban/Assets/MyScripts/AI.cs
+++ Sokoban/Assets/MyScripts/AI.cs
@@ -20,154 +20,152 @@ public class AI : MonoBehaviour {
         //Debug.Log("Size: " + m.Count);
         //foreach(Vector3 v in m) { AIMove(v); }
         //InvokeRepeating("AIMove(m[0])", 2, 2);
-        AIMove();
+        //AIMove();
     }
 
-    public void AIMove()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            InitialState = getCurrentState();
+    //public void AIMove()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        InitialState = getCurrentState();
 
-            List<Vector3> m = (GenerateListOfMoves(InitialState, 0));
-            Debug.Log(m[0]);
-            transform.position += m[0];
-            InitialState = getCurrentState();
-        }
+    //        List<Vector3> m = (GenerateListOfMoves(InitialState, 0));
+    //        Debug.Log(m[0]);
+    //        transform.position += m[0];
+    //        InitialState = getCurrentState();
+    //    }
             
-    }
-    VirtualState getCurrentState()
-    {
-        VirtualState CurrentState = new VirtualState(
-            GameObject.FindGameObjectsWithTag("Box"),
-            GameObject.FindGameObjectsWithTag("Cross"),
-            GameObject.FindGameObjectWithTag("Player"),
-            new Vector3(0, 0, 0)
-        );
-        Debug.Log("Initial position: " + GameObject.FindGameObjectWithTag("Player").transform.position);
+    //}
+    //VirtualState getCurrentState()
+    //{
+    //    VirtualState CurrentState = new VirtualState(
+    //        GameObject.FindGameObjectsWithTag("Box"),
+    //        GameObject.FindGameObjectsWithTag("Cross"),
+    //        GameObject.FindGameObjectWithTag("Player"),
+    //        new Vector3(0, 0, 0)
+    //    );
+    //    Debug.Log("Initial position: " + GameObject.FindGameObjectWithTag("Player").transform.position);
 
-        return CurrentState;
-    }
-    float CreateSearchTree(VirtualState state, int n, ref List<Vector3> moves)
-    {
-        List<VirtualState> PossibleStates = GeneratePossibleStates(state);
-        VirtualState best = new VirtualState();
-        foreach (VirtualState v in PossibleStates) {
-            if (n + 1 < limit)
-            {
-                v.EvaluationValue += CreateSearchTree(v, n + 1, ref moves);
-            }
-            if (v.EvaluationValue < best.EvaluationValue)
-            {
-                best = v;
-            }
-        }
-        //Debug.Log("N: " + n + " , " + best.EvaluationValue + ", " + best.move);
+    //    return CurrentState;
+    //}
+    //float CreateSearchTree(VirtualState state, int n, ref List<Vector3> moves)
+    //{
+    //    List<VirtualState> PossibleStates = GeneratePossibleStates(state);
+    //    VirtualState best = new VirtualState();
+    //    foreach (VirtualState v in PossibleStates) {
+    //        if (n + 1 < limit)
+    //        {
+    //            v.EvaluationValue += CreateSearchTree(v, n + 1, ref moves);
+    //        }
+    //        if (v.EvaluationValue < best.EvaluationValue)
+    //        {
+    //            best = v;
+    //        }
+    //    }
+    //    //Debug.Log("N: " + n + " , " + best.EvaluationValue + ", " + best.move);
 
-        if( n ==0 )
-            moves.Add(best.move);
+    //    if( n ==0 )
+    //        moves.Add(best.move);
 
-        return best.EvaluationValue; 
-    }
-    List<Vector3> GenerateListOfMoves(VirtualState state, int n)
-    {
-        List<Vector3> moves = new List<Vector3>();
-        CreateSearchTree(state, n, ref moves);
-        Debug.Log(moves[0]);
-        return moves;
-    }    
-    List<VirtualState> GeneratePossibleStates(VirtualState state)
-    {
-        List<VirtualState> newStates = new List<VirtualState>();
-        //Possible movement of player, and possible change of box pos
-        GameObject Player = Instantiate(state.Player, state.Player.transform.position, Quaternion.identity) as GameObject;
-        GameObject[] Boxes = state.Boxes;
-        Vector3 currentPos = Player.transform.position;
+    //    return best.EvaluationValue; 
+    //}
+    //List<Vector3> GenerateListOfMoves(VirtualState state, int n)
+    //{
+    //    List<Vector3> moves = new List<Vector3>();
+    //    CreateSearchTree(state, n, ref moves);
+    //    Debug.Log(moves[0]);
+    //    return moves;
+    //}    
+    //List<VirtualState> GeneratePossibleStates(VirtualState state) 
+    //{
+    //    List<VirtualState> newStates = new List<VirtualState>();
+    //    //Possible movement of player, and possible change of box pos
+    //    GameObject Player = Instantiate(state.Player, state.Player.transform.position, Quaternion.identity) as GameObject;
+    //    GameObject[] Boxes = state.Boxes;
+    //    Vector3 currentPos = Player.transform.position;
         
-        List<Vector3> listOfPossibleMoves = possibleMoves(state);
-        foreach (Vector3 move in listOfPossibleMoves) {
-            //Player.transform.position = currentPos;
-            //VirtualState temporaryState = new VirtualState(state.Boxes, state.Crosses, state.Player, state.move);
-            //temporaryState.Player.transform.position += move;
-            Player.transform.position += move;
+    //    List<Vector3> listOfPossibleMoves = possibleMoves(state);
+    //    foreach (Vector3 move in listOfPossibleMoves) {
+    //        Player.transform.position = currentPos;
+    //        //VirtualState temporaryState = new VirtualState(state.Boxes, state.Crosses, state.Player, state.move);
+    //        //temporaryState.Player.transform.position += move;
+    //        Player.transform.position += move;
             
-            //Can Be problem with boxes position. 
-            //If Box at move position change box position
-            if (PlayerController.BoxAtPos(Boxes, Player.transform.position + move))
-            {
-                //newStates.Add(CreateState(state, move, PlayerController.iBoxAtPos(Boxes, Player.transform.position + move)));
-                //Boxes[PlayerController.iBoxAtPos(Boxes, Player.transform.position + move)].transform.position += move;                
-            }
-            else
-            {
-                //newStates.Add(CreateState(temporaryState, move));
-            }
-        }
-        return newStates;
-    }
+    //        //Can Be problem with boxes position. 
+    //        //If Box at move position change box position
+    //        if (PlayerController.BoxAtPos(Boxes, Player.transform.position + move))
+    //        {
+    //            //newStates.Add(CreateState(state, move, PlayerController.iBoxAtPos(Boxes, Player.transform.position + move)));
+    //            //Boxes[PlayerController.iBoxAtPos(Boxes, Player.transform.position + move)].transform.position += move;                
+    //        }
+    //        else
+    //        {
+    //            //newStates.Add(CreateState(temporaryState, move));
+    //        }
+    //    }
+    //    return newStates;
+    //}
+    //VirtualState CreateState(VirtualState state, Vector3 move)
+    //{
+    //    state.Player.transform.position += move;
+    //    return (new VirtualState(
+    //        state.Boxes,
+    //        state.Crosses,
+    //        state.Player,
+    //        move));
+    //}
+    //List<Vector3> possibleMoves(VirtualState state)
+    //{
+    //    List<Vector3> possibleMoves = new List<Vector3>();
 
-    VirtualState CreateState(VirtualState state, Vector3 move)
-    {
-        state.Player.transform.position += move;
-        return (new VirtualState(
-            state.Boxes,
-            state.Crosses,
-            state.Player,
-            move));
-    }
-
-    List<Vector3> possibleMoves(VirtualState state)
-    {
-        List<Vector3> possibleMoves = new List<Vector3>();
-
-        Vector3 v = state.Player.transform.position + new Vector3(1, 0, 0);
-        if (PlayerController.MoveIsPossible(
-            state.Player.transform.position,
-            v,
-            state.Boxes))
-        {
-            possibleMoves.Add(new Vector3(1, 0, 0));
-        }
+    //    Vector3 v = state.Player.transform.position + new Vector3(1, 0, 0);
+    //    if (PlayerController.MoveIsPossible(
+    //        state.Player.transform.position,
+    //        v,
+    //        state.Boxes))
+    //    {
+    //        possibleMoves.Add(new Vector3(1, 0, 0));
+    //    }
 
 
-        v = state.Player.transform.position + new Vector3(-1, 0, 0);
-        if (PlayerController.MoveIsPossible(
-            state.Player.transform.position,
-            v,
-            state.Boxes))
-        {
-            possibleMoves.Add(new Vector3(-1, 0, 0));
-        }
-        v = state.Player.transform.position + new Vector3(0, 1, 0);
-        if (PlayerController.MoveIsPossible(
-            state.Player.transform.position,
-            v,
-            state.Boxes))
-        {
-            possibleMoves.Add(new Vector3(0, 1, 0));
-        }
-        v = state.Player.transform.position + new Vector3(0, -1, 0);
-        if (PlayerController.MoveIsPossible(
-            state.Player.transform.position,
-            v,
-            state.Boxes))
-        {
-            possibleMoves.Add(new Vector3(0, -1, 0));
-        }
+    //    v = state.Player.transform.position + new Vector3(-1, 0, 0);
+    //    if (PlayerController.MoveIsPossible(
+    //        state.Player.transform.position,
+    //        v,
+    //        state.Boxes))
+    //    {
+    //        possibleMoves.Add(new Vector3(-1, 0, 0));
+    //    }
+    //    v = state.Player.transform.position + new Vector3(0, 1, 0);
+    //    if (PlayerController.MoveIsPossible(
+    //        state.Player.transform.position,
+    //        v,
+    //        state.Boxes))
+    //    {
+    //        possibleMoves.Add(new Vector3(0, 1, 0));
+    //    }
+    //    v = state.Player.transform.position + new Vector3(0, -1, 0);
+    //    if (PlayerController.MoveIsPossible(
+    //        state.Player.transform.position,
+    //        v,
+    //        state.Boxes))
+    //    {
+    //        possibleMoves.Add(new Vector3(0, -1, 0));
+    //    }
 
-        if (possibleMoves.Count == 0) throw new System.ArgumentException("There are not possible moves");
-        return possibleMoves;
-    }
+    //    if (possibleMoves.Count == 0) throw new System.ArgumentException("There are not possible moves");
+    //    return possibleMoves;
+    //}
 }
 
 public class VirtualState
 {
-    public GameObject[] Boxes, Crosses;
-    public GameObject Player;
+    public Vector3[] Boxes, Crosses;
+    public Vector3 Player;
     public float EvaluationValue;
     public Vector3 move;
 
-    public VirtualState(GameObject[]Boxes, GameObject[] Crosses, GameObject Player, Vector3 move)
+    public VirtualState(Vector3[]Boxes, Vector3[] Crosses, Vector3 Player, Vector3 move)
     {
         this.Boxes = Boxes;
         this.Crosses = Crosses;
@@ -194,7 +192,7 @@ public class VirtualState
         this.move = state.move;*/
     }
 
-    public static float EvaluationFuction(GameObject[] Boxes, GameObject[] Crosses, GameObject Player)
+    public static float EvaluationFuction(Vector3[] Boxes, Vector3[] Crosses, Vector3 Player)
     {
 
         float value = 0;
@@ -208,11 +206,11 @@ public class VirtualState
 
     }
 
-    static float CalculateDistanceBeetwenObjects(GameObject ob1, GameObject ob2)
+    static float CalculateDistanceBeetwenObjects(Vector3 ob1, Vector3 ob2)
     {
         return Mathf.Sqrt(
-                        Mathf.Pow(ob1.transform.position.x - ob2.transform.position.x, 2)
-                        + Mathf.Pow(ob1.transform.position.y - ob2.transform.position.y, 2)
+                        Mathf.Pow(ob1.x - ob2.x, 2)
+                        + Mathf.Pow(ob1.y - ob2.y, 2)
                         );
     }
 }
